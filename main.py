64a5a86db54bf32ef382f9610cb61950cb265859
Lee Penkman <leepenkman@gmail.com>
2025-04-17 15:23:39 +1200

    wip

--- main.py
+++ main.py
@@ -1,47 +1,39 @@
 # !/usr/bin/env python
 import os
-
-import webapp2
+# Migrated to FastAPI from webapp2
+from fastapi import FastAPI, Request  # type: ignore
+from fastapi.responses import HTMLResponse, RedirectResponse  # type: ignore
+from fastapi.staticfiles import StaticFiles  # type: ignore
 import jinja2
 
-
+# Jinja2 environment remains same as before
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'],
+    extensions=[],
     autoescape=True)
+
+# Original config for webapp2 sessions remains for reference (not used in FastAPI)
 config = {'webapp2_extras.sessions': dict(secret_key='93986c9cdd240540f70efaea56a9e3f2')}
 
+app = FastAPI()
 
-class BaseHandler(webapp2.RequestHandler):
-    def render(self, view_name, extraParams={}):
-        template_values = {
-        }
-        template_values.update(extraParams)
+# Mount static directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-        template = JINJA_ENVIRONMENT.get_template(view_name)
-        self.response.write(template.render(template_values))
+@app.get("/", response_class=HTMLResponse)
+async def main(request: Request):
+    # Render index.html template for the root URL
+    template = JINJA_ENVIRONMENT.get_template('index.html')
+    return HTMLResponse(content=template.render({}))
 
-
-class MainHandler(BaseHandler):
-    def get(self):
-        self.render('index.html')
-
-
-class NotFoundHandler(BaseHandler):
-    def get(self):
-        self.response.set_status(404)
-        self.render('index.html')
-
-
-class SlashMurdererApp(webapp2.RequestHandler):
-    def get(self, url):
-        self.redirect(url)
-
-
-app = webapp2.WSGIApplication([
-                                  ('/', MainHandler),
-                                  ('(.*)/$', SlashMurdererApp),
-                              ] + [
-                                  ('/.*', NotFoundHandler),
-                              ]
-                              , debug=os.environ.get('SERVER_SOFTWARE', '').startswith('Development/'), config=config)
+@app.get("/{full_path:path}", response_class=HTMLResponse)
+async def catch_all(request: Request, full_path: str):
+    # If the URL ends with a slash, redirect to the non-slash version
+    if request.url.path.endswith("/") and request.url.path != "/":
+        new_url = request.url.path.rstrip("/")
+        if request.url.query:
+            new_url += "?" + request.url.query
+        return RedirectResponse(url=new_url)
+    # For all other routes, respond with a 404 and render index.html
+    template = JINJA_ENVIRONMENT.get_template('index.html')
+    return HTMLResponse(content=template.render({}), status_code=404)
